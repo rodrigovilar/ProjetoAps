63ae456014bbfdb473097db210d665695acd899a
Gilvando Lucas Vieira <gilvandolucas@gmail.com>
2013-05-30 14:03:20 -0300

    CRUD produtos

--- src/br/ufpb/dce/aps/GerenteDeProdutos.java
+++ src/br/ufpb/dce/aps/GerenteDeProdutos.java
@@ -2,9 +2,19 @@ package br.ufpb.dce.aps;
 
 import java.util.ArrayList;
 
+/*
+ * 
+ * Gerando CRUD
+ * C => Create
+ * R => Read
+ * U => Update
+ * D => Delete
+ * 
+ * */
 public class GerenteDeProdutos {
 	ArrayList<Produto>	produtos	= new ArrayList<Produto>();
 	
+	// Create
 	public boolean adicionarProduto(int c, int q, int t, String nome) {
 		Produto p = new Produto(c, q, t, nome);
 		if (!(produtos.contains(p))) {
@@ -15,6 +25,31 @@ public class GerenteDeProdutos {
 		}
 	}
 	
+	// Read
+	public Produto getProduto(int c) {
+		Produto p = new Produto(c);
+		if (produtos.contains(p)) {
+			return produtos.get(produtos.indexOf(p));
+		}
+		else
+			return null;
+	}
+	
+	// Update
+	public boolean setProduto(int c, int q, int t, String nome) {
+		Produto p = new Produto(c);
+		if (produtos.contains(p)) {
+			produtos.get(produtos.indexOf(p)).setNome(nome);
+			produtos.get(produtos.indexOf(p)).setQuantidade(q);
+			produtos.get(produtos.indexOf(p)).setTipo(t);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	// Delete
 	public boolean removerProduto(int c) {
 		Produto p = new Produto(c);
 		if (produtos.contains(p)) {
