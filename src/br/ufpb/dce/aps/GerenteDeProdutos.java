f5c530beb416d52faceaacdd2ce4b133fbf1b1e5
Gilvando Lucas Vieira <gilvandolucas@gmail.com>
2013-05-30 13:44:22 -0300

    Funcao adiciona e remove do estoque

--- src/br/ufpb/dce/aps/GerenteDeProdutos.java
+++ src/br/ufpb/dce/aps/GerenteDeProdutos.java
@@ -1,5 +1,34 @@
 package br.ufpb.dce.aps;
 
+import java.util.ArrayList;
+
 public class GerenteDeProdutos {
+	ArrayList<Produto>	produtos	= new ArrayList<Produto>();
+	
+	public boolean adicionarProduto(int c, int q, int t, String nome) {
+		Produto p = new Produto(c, q, t, nome);
+		if (!(produtos.contains(p))) {
+			return produtos.add(p);
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean removerProduto(int c) {
+		Produto p = new Produto(c);
+		if (produtos.contains(p)) {
+			produtos.remove(produtos.indexOf(p));
+			if (!(produtos.contains(p))) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
 	
 }
--- src/br/ufpb/dce/aps/Produto.java
+++ src/br/ufpb/dce/aps/Produto.java
@@ -3,11 +3,18 @@ package br.ufpb.dce.aps;
 public class Produto {
 	private int			codigo;
 	private int			quantidade;
+	private int			tipo;
 	private String	nome;
 	
-	public Produto(int c, String nome) {
+	public Produto(int c, int q, int t, String nome) {
 		this.codigo = c;
+		this.quantidade = q;
+		this.tipo = t;
 		this.nome = nome;
+	}
+	
+	public Produto(int c) {
+		this.codigo = c;
 	}
 	
 	public int getCodigo() {
@@ -25,13 +32,29 @@ public class Produto {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	
 	public int getQuantidade() {
 		return quantidade;
 	}
-
+	
 	public void setQuantidade(int quantidade) {
 		this.quantidade = quantidade;
 	}
 	
+	public int getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	
+	public boolean equals(Object o) {
+		if (o instanceof Produto) {
+			return (((Produto) o).getCodigo() == this.getCodigo());
+		}
+		else {
+			return false;
+		}
+	}
 }
