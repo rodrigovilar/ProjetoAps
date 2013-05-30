4bd4388842451893a88389f50572dc46722abf12
Gilvando Lucas Vieira <gilvandolucas@gmail.com>
2013-05-30 14:13:02 -0300

    Iniciando Testes

--- src/br/ufpb/dce/aps/ClasseDeTeste.java
+++ src/br/ufpb/dce/aps/ClasseDeTeste.java
@@ -1,5 +1,33 @@
 package br.ufpb.dce.aps;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ClasseDeTeste {
+	GerenteDeProdutos	g;
 	
+	@Before
+	public void setUp() {
+		g = new GerenteDeProdutos();
+	}
+	
+	@Test
+	public void adicionaProduto() {
+		assertTrue(g.adicionarProduto(0, 0, 0, "Laptop"));
+	}
+	
+	@Test
+	public void getProduto(){
+		Produto p = g.getProduto(0);
+		assertEquals(p, g.getProduto(0));
+	}
+	
+	@Test
+	public void setProduto(){
+		Produto p = g.getProduto(0);
+		Produto m = g.getProduto(0);
+		p.setNome("Notebook");
+		assertEquals(m, p);
+	}
 }
