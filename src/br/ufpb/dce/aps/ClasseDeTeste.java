f9cb37f1cd53993ece5a36607b75b1e79e93d286
Gilvando Lucas Vieira <gilvandolucas@gmail.com>
2013-06-03 09:56:37 -0300

    Tirando os prints

--- src/br/ufpb/dce/aps/ClasseDeTeste.java
+++ src/br/ufpb/dce/aps/ClasseDeTeste.java
@@ -15,8 +15,6 @@ public class ClasseDeTeste {
 	@Test
 	public void adicionaProduto() {
 		assertTrue(g.adicionarProduto(0, 0, 0, "Laptop"));
-		System.out.println("Adicionado com sucesso! " + g.getProduto(0));
-		System.out.println("----------------------------------------------");
 	}
 	
 	@Test
@@ -24,8 +22,6 @@ public class ClasseDeTeste {
 		g.adicionarProduto(0, 0, 0, "Laptop");
 		Produto p = g.getProduto(0);
 		assertEquals(p, g.getProduto(0));
-		System.out.println("Lido com sucesso! " + p);
-		System.out.println("----------------------------------------------");
 	}
 	
 	@Test
@@ -34,12 +30,6 @@ public class ClasseDeTeste {
 		Produto p = new Produto(0);
 		p.setNome("Notebook");
 		assertEquals(g.getProduto(0), p);
-		// -------------------------------------
-		// update produto
-		System.out.println("Update nome produto @test ok com mesmo código");
-		System.out.println("Print do objeto local => " + p);
-		System.out.println("Print da lista global => " + g.getProduto(0));
-		System.out.println("----------------------------------------------");
 	}
 	
 	@Test
@@ -47,9 +37,5 @@ public class ClasseDeTeste {
 		g.adicionarProduto(0, 0, 0, "Laptop");
 		Produto p = new Produto(0);
 		assertFalse(p.equals(g.getProduto(1)));
-		System.out.println("@test ok com codigos diferentes");
-		System.out.println("Print do objeto local => " + p);
-		System.out.println("Print da lista global => " + g.getProduto(0));
-		System.out.println("----------------------------------------------");
 	}
 }
