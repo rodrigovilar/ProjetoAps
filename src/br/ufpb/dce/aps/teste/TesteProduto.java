30165280dcd913cf4a3ac84aefe6f72ea4992bc7
Vinicius de Andrade <vincius.andrade@dce.ufpb.br>
2013-06-10 09:21:16 -0300

    Adiciona mais testes

--- src/br/ufpb/dce/aps/teste/TesteProduto.java
+++ src/br/ufpb/dce/aps/teste/TesteProduto.java
@@ -7,25 +7,40 @@ import org.junit.Test;
 import br.ufpb.dce.aps.projeto.*;
 
 public class TesteProduto {
-	Produto p ;
+	Produto p;
 	ControleProduto cp;
 
-
 	@Before
-	public void setUp(){
-		p= new Produto();
+	public void setUp() {
+		p = new Produto();
 		cp = new ControleProduto();
 	}
-	
+
 	@Test
-	public void addProduto(){
+	public void criaProduto() {
 		p.setCodigo(1);
 		p.setNome("Vinicius");
 		p.setPreco(40);
 		assertEquals(1, p.getCodigo());
-		assertEquals("Vinicius", p.getNome());
-		assertTrue(40==p.getPreco());
-		assertEquals(40,p.getPreco(), 0.1);
 	}
-	
+
+	@Test
+	public void verSeProdutoTemNome() {
+		this.criaProduto();
+		assertEquals("Vinicius", p.getNome());
+	}
+
+	@Test
+	public void verificaPrecoDoProduto() {
+		this.criaProduto();
+		assertEquals(40, p.getPreco(), 0.1);
+	}
+
+	@Test
+	public void addProdutoNaLista() {
+		cp.cadastrarProduto(p.getNome(), p.getCodigo(), p.getPreco());
+		assertEquals(p.getNome(), cp.buscarProduto(p.getCodigo()).getNome());
+
+	}
+
 }
