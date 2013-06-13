d190dca720ac578e35a9b3f5d276c8fcc5d3f1e6
Vinicius de Andrade <vincius.andrade@dce.ufpb.br>
2013-06-13 10:11:33 -0300

    primeito teste do cliente feito!

--- src/br/ufpb/dce/aps/teste/TesteCliente.java
+++ src/br/ufpb/dce/aps/teste/TesteCliente.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import br.ufpb.dce.aps.controles.ControleProduto;
 import br.ufpb.dce.aps.entidades.Produto;
+import br.ufpb.dce.aps.exception.ClienteJaCadastradoException;
 import br.ufpb.dce.aps.exception.ProdutoJaCadastradoException;
 import br.ufpb.dce.aps.fachada.FachadaFiado;
 import br.ufpb.dce.aps.entidades.Produto;
@@ -22,15 +23,31 @@ import br.ufpb.dce.aps.fachada.FachadaFiado;
 public class TesteCliente {
 
 	public FachadaFiado ff ;
-	
+	// contantes para endereco e contato
+	public static final String BAIRRO = "centro";
+	public static final String RUA = "quintaAvenida";
+	public static final String NUMERO = "1000";
+	public static final String TELEFONE= "9999-9999";
+	public static final String REFERENCIA = "perto da farmacia";
+	public static final String CPF = "098765432-12";
+	public static final String NOME = "Vinicius";
+
 	@Before
 	public void setUp(){
-	ff= new FachadaFiado();
+		ff= new FachadaFiado();
+
+		try {
+			// cadastrando cliente
+			ff.ControleCliente().cadastrarCliente(this.NUMERO, this.NOME, this.RUA,this.BAIRRO, this.TELEFONE, this.REFERENCIA, this.CPF);
+		} catch (ClienteJaCadastradoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	@Test
-	public void testeCadastraCliente(){
-		
+	public void testeClienteCadastrado(){
+		assertEquals(this.NOME, ff.ControleCliente().buscarCliente(this.CPF).getNome());
 	}
 
 }
