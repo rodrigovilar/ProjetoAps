cc72bdefeb1b3a8359524370c4574fd0a4ee797c
Gilvando Lucas Vieira <gilvandolucas@gmail.com>
2013-06-17 16:33:57 -0300

    rebase

--- src/br/ufpb/dce/aps/entidades/User.java
+++ src/br/ufpb/dce/aps/entidades/User.java
@@ -1,25 +1,9 @@
 package br.ufpb.dce.aps.entidades;
-public class User{
+public class Login {
 
-	private String username;
+	private String login;
 
-	private String password;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	private String senha;
 	
 	
 
