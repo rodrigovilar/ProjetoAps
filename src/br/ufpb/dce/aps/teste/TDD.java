10524c9792c90b3d145b0d749e0a52bc830a854d
Gilvando Lucas Vieira <gilvandolucas@gmail.com>
2013-06-17 15:44:53 -0300

    criando classe TDD

---
+++ src/br/ufpb/dce/aps/teste/TDD.java
@@ -0,0 +1,19 @@
+package br.ufpb.dce.aps.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TDD {
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void test_001(){
+		assertEquals(this, this);
+	}
+	
+}
