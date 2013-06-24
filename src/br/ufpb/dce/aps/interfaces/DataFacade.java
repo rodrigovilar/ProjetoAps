b96bd5b159f6a1700bb3b5f0ac513f7eba266327
Gilvando Lucas Vieira <gilvandolucas@gmail.com>
2013-06-24 10:32:59 -0300

    revertendo (problemas com design)

--- src/br/ufpb/dce/aps/interfaces/DataFacade.java
+++
@@ -1,7 +0,0 @@
-package br.ufpb.dce.aps.interfaces;
-
-public interface DataFacade {
-	
-	public boolean login(String username, String password);
-
-}
--- src/br/ufpb/dce/aps/teste/TDD.java
+++ src/br/ufpb/dce/aps/teste/TDD.java
@@ -1,36 +1,38 @@
 package br.ufpb.dce.aps.teste;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import br.ufpb.dce.aps.entidades.Session;
 import br.ufpb.dce.aps.exception.PasswordInvalidException;
 import br.ufpb.dce.aps.exception.UsernameInvalidException;
+import br.ufpb.dce.aps.fachada.FachadaFiado;
 
 public class TDD {
+	public FachadaFiado	f;
 	
 	@Before
 	public void setUp() {
+		f = new FachadaFiado();
 	}
 	
 	@Test
 	public void test_001() {
 		/* Login */
-		assertTrue(Session.login("admin", " "));
+		assertEquals(null, f.login("admin", " "));
 	}
 	
 	@Test(expected = UsernameInvalidException.class)
 	public void test_002() {
 		/* Login com exception em username */
-		assertTrue(Session.login("admina", " "));
+		assertEquals(null, f.login("admina", " "));
 	}
 	
 	@Test(expected = PasswordInvalidException.class)
 	public void test_003() {
 		/* Login com exception em password */
-		assertTrue(Session.login("admin", "1"));
+		assertEquals(null, f.login("admin", "1"));
 	}
 	
 }
