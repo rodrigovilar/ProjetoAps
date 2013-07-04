9dc94826388eed168addce13f9b65dcc403eb38a
Vinicius de Andrade <vincius.andrade@dce.ufpb.br>
2013-07-04 15:05:49 -0300

    fazendo garantias de entradas dos parâmetros

--- src/br/ufpb/dce/aps/controles/ControleProduto.java
+++ src/br/ufpb/dce/aps/controles/ControleProduto.java
@@ -1,69 +1,94 @@
 package br.ufpb.dce.aps.controles;
 
 import java.util.LinkedList;
+import java.util.regex.Pattern;
 import java.util.List;
 
 import br.ufpb.dce.aps.entidades.Produto;
 import br.ufpb.dce.aps.exception.ProdutoJaCadastradoException;
+import br.ufpb.dce.aps.exception.ValorInvalidoException;
 
 public class ControleProduto {
 
 	private List<Produto> estoque = new LinkedList<Produto>();
 	private Produto p;
-	private int contadorDeProduto = 0;
+
 
 	public void cadastrarProduto(String nome, int codigo, float preco)
-			throws ProdutoJaCadastradoException {
+			throws ProdutoJaCadastradoException, ValorInvalidoException {
 		p = new Produto();
-		// se lista vazia
-		if (this.estoque.isEmpty()) {
-			p.setNome(nome);
-			p.setPreco(preco);
-			p.setCodigo(codigo);
-			this.estoque.add(p);
-			this.contadorDeProduto++;
 
-			// se lista não vazia, cheque se os produtos já existente
-		} else if (codigo != this.buscarProduto(codigo).getCodigo()) {
-			p.setNome(nome);
-			p.setPreco(preco);
-			p.setCodigo(codigo);
-			this.contadorDeProduto++;
-			this.estoque.add(p);
-			
-			// se produto ja existe
-		} else {
-			throw new ProdutoJaCadastradoException("Produto Exception");
-		}
+		if(isValido(nome, codigo, preco)) {
+			// se lista vazia
+			if (this.estoque.isEmpty()) {
+				p.setNome(nome);
+				p.setPreco(preco);
+				p.setCodigo(codigo);
+				this.estoque.add(p);			
+
+				// se lista não vazia, cheque se os produtos já existente
+			} else if (codigo != this.buscarProduto(codigo).getCodigo()) {
+				p.setNome(nome);
+				p.setPreco(preco);
+				p.setCodigo(codigo);			
+				this.estoque.add(p);
+
+				// se produto ja existe
+			} else 
+				throw new ProdutoJaCadastradoException("Produto Exception");
+
+		}else throw new ValorInvalidoException("Valor Inesperado");
+
 	}
 
 	public boolean removerProduto(int codigo) {
 		Produto p = this.buscarProduto(codigo);
 		if (p != null) {
-			this.estoque.remove(p);
-			this.contadorDeProduto--;
+			this.estoque.remove(p);			
 			return true;
 		}
 		return false;
 	}
 
-	
 
-	public Produto buscarProduto(int codigo) {
-		for (Produto p : estoque)
-			if (p.getCodigo() == codigo)
-				return p;
-		return null;
+
+	public Produto buscarProduto(int codigo) throws ValorInvalidoException{
+		if(!this.isValido(codigo))
+			throw new ValorInvalidoException("valor invalido");
+		else{
+			for (Produto p : estoque)
+				if (p.getCodigo() == codigo)
+					return p;
+			return null;
+		}
 	}
-	
 	public int getNumeroDeProdutos(){
-		return this.contadorDeProduto;
+		return this.estoque.size();
 	}
 
 	public List<Produto> exibirEstoqueDeProdutos() {
 		if(!this.estoque.isEmpty())
-		return this.estoque;
+			return this.estoque;
 		return null;
 	}
 
+
+	private boolean isValido(String nome, int codigo, float preco) {
+		// testador de entrada de parâmetros
+		String cod = String.valueOf(codigo);
+		String prec = String.valueOf(preco);
+
+		if((Pattern.matches("[a-zA-Z]", nome)) && (Pattern.matches("[0-9]", cod)) && (Pattern.matches("[0-9]",prec)))
+			return true;
+		return false;
+	}
+
+	private boolean isValido(int codigo){
+		// testador de entrada de parâmetros para busca e remoção
+		String cod = String.valueOf(codigo);
+		if((Pattern.matches("[0-9]", cod)))
+			return true;
+		return false;
+
+	}
 }
---
+++ src/br/ufpb/dce/aps/exception/ValorInvalidoException.java
@@ -0,0 +1,9 @@
+package br.ufpb.dce.aps.exception;
+
+public class ValorInvalidoException extends RuntimeException {
+	
+	
+	public ValorInvalidoException(String msg){
+		super(msg);
+	}
+}
